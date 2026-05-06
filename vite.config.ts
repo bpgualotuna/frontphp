577cab843cc8ef50aeacd041c08f00867ec72841
bpgualotuna <brayan.gualotuna09@gmail.com>
2026-05-06 11:15:37 -0500

    actualizar

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    strictPort: false,
+  },
 })
